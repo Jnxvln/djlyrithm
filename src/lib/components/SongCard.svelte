be726ba4f55161c430d851d377bf0e92e0d8777c
Justin Cox <Justincox.dev@gmail.com>
2025-06-09 22:28:43 -0500

    Add target _blank and code cleanup

--- src/lib/components/SongCard.svelte
+++ src/lib/components/SongCard.svelte
@@ -4,7 +4,7 @@
 </script>
 
 <!-- STYLE 1 -->
-<a href={song.link}>
+<a href={song.link} target="_blank">
 	<div
 		class="flex w-full max-w-sm flex-col overflow-hidden rounded-xl bg-white bg-opacity-5 p-4 shadow-lg backdrop-blur transition-transform hover:scale-105"
 	>
@@ -32,25 +32,3 @@
 		</div>
 	</div>
 </a>
-
-<!-- Song Card (JC ORIG) -->
-<!-- <div class="mb-8 flex w-full flex-col flex-wrap">
-	<div>
-		<img
-			class="h-64 w-full object-cover"
-			src={song.image}
-			alt={`Image for the song "${song.title}"`}
-		/>
-		<div class="text-2xl font-bold">{song.title}</div>
-		<div class="text-sm text-gray-500">Duration: {song.duration}</div>
-
-		<div class="flex items-center">
-			<div class="text-sm text-gray-300">Tags:</div>
-			<span class="ml-2">
-				{#each song.tags as tag (tag)}
-					<span class="mr-2 text-sm text-gray-500">{tag}</span>
-				{/each}
-			</span>
-		</div>
-	</div>
-</div> -->
