bb87aebb9510ed7ffe56693d97ff1052640beb64
Justin Cox <Justincox.dev@gmail.com>
2025-06-25 19:28:11 -0500

    Update songs and resize RedHollow song image

--- src/lib/utils/sortSongs.ts
+++ src/lib/utils/sortSongs.ts
@@ -6,14 +6,21 @@ import type { Song } from '$lib/types/songs';
  * @returns The sorted array of songs in ascending order by date published
  */
 export const sortSongsByDateAscending = (songs: Song[]): Song[] => {
-	return songs.sort((a, b) => a.datePublished.getTime() - b.datePublished.getTime());
+	return songs.sort(
+		(a, b) => new Date(a.datePublished).getTime() - new Date(b.datePublished).getTime()
+	);
 };
 
 /**
  * Sort Songs by Date Descending
  * @param songs The array of songs to be sorted
- * @returns The sorted array of songs in descending order by date published
+	return songs.sort(
+		(a, b) =>
+			new Date(b.datePublished).getTime() - new Date(a.datePublished).getTime()
+	);
  */
 export const sortSongsByDateDescending = (songs: Song[]): Song[] => {
-	return songs.sort((a, b) => b.datePublished.getTime() - a.datePublished.getTime());
+	return songs.sort(
+		(a, b) => new Date(b.datePublished).getTime() - new Date(a.datePublished).getTime()
+	);
 };
